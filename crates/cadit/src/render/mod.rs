fa3afc8e69af1d24b8cf643fac480ca387fbaef3
Ross Tollefson <past9sys@gmail.com>
2023-01-01 00:09:48 -0700

    PbrScene

--- crates/cadit/src/render/mod.rs
+++ crates/cadit/src/render/mod.rs
@@ -1,1 +1,42 @@
+use std::sync::Arc;
 
+use eframe::epaint::PaintCallbackInfo;
+use egui_winit_vulkano::RenderResources;
+use vulkano::{image::ImageViewAbstract, pipeline::graphics::viewport::Viewport};
+
+mod scene;
+
+trait Scene {
+    fn render<'a>(&mut self, info: &PaintCallbackInfo, resources: &RenderResources<'a>);
+    fn view(&self) -> Arc<dyn ImageViewAbstract>;
+}
+
+#[derive(PartialEq)]
+pub struct SceneViewport {
+    origin: [u32; 2],
+    dimensions: [u32; 2],
+}
+impl SceneViewport {
+    fn zero() -> Self {
+        Self {
+            origin: [0, 0],
+            dimensions: [0, 0],
+        }
+    }
+
+    fn from_info(info: &PaintCallbackInfo) -> Self {
+        let vp = info.viewport_in_pixels();
+        Self {
+            origin: [vp.left_px as u32, vp.top_px as u32],
+            dimensions: [vp.width_px as u32, vp.height_px as u32],
+        }
+    }
+
+    fn to_vulkan_viewport(&self) -> Viewport {
+        Viewport {
+            origin: [self.origin[0] as f32, self.origin[1] as f32],
+            dimensions: [self.dimensions[0] as f32, self.dimensions[1] as f32],
+            depth_range: 0.0..1.0,
+        }
+    }
+}
---
+++ crates/cadit/src/render/scene.rs
@@ -0,0 +1,336 @@
+use std::sync::Arc;
+
+use bytemuck::{Pod, Zeroable};
+use eframe::epaint::PaintCallbackInfo;
+use egui_winit_vulkano::RenderResources;
+use vulkano::{
+    buffer::{BufferUsage, CpuAccessibleBuffer, TypedBufferAccess},
+    command_buffer::{
+        AutoCommandBufferBuilder, CommandBufferUsage, PrimaryCommandBufferAbstract,
+        RenderPassBeginInfo, SubpassContents,
+    },
+    format::Format,
+    image::{
+        view::ImageView, AttachmentImage, ImageDimensions, ImageViewAbstract, SampleCount,
+        StorageImage,
+    },
+    pipeline::{
+        graphics::{
+            depth_stencil::{CompareOp, DepthState, DepthStencilState},
+            input_assembly::{InputAssemblyState, PrimitiveTopology},
+            multisample::MultisampleState,
+            rasterization::{CullMode, FrontFace, RasterizationState},
+            vertex_input::BuffersDefinition,
+            viewport::ViewportState,
+        },
+        GraphicsPipeline, StateMode,
+    },
+    render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass, Subpass},
+    sync::GpuFuture,
+};
+
+use super::{Scene, SceneViewport};
+
+const MSAA_SAMPLES: SampleCount = SampleCount::Sample8;
+const IMAGE_FORMAT: Format = Format::B8G8R8A8_UNORM;
+
+pub struct PbrScene {
+    clear_color: [f32; 4],
+    render_pass: Arc<RenderPass>,
+    pipeline: Arc<GraphicsPipeline>,
+    subpass: Subpass,
+    viewport: SceneViewport,
+    vertex_buffer: Arc<CpuAccessibleBuffer<[SceneVertex]>>,
+    images: PbrSceneImages,
+    msaa_samples: SampleCount,
+}
+impl PbrScene {
+    pub fn new<'a>(
+        clear_color: [f32; 4],
+        resources: &RenderResources<'a>,
+        msaa_samples: SampleCount,
+    ) -> Self {
+        let device = resources.queue.device().clone();
+
+        let viewport = SceneViewport::zero();
+
+        let render_pass = vulkano::single_pass_renderpass!(
+            device.clone(),
+            attachments: {
+                intermediary: {
+                    load: Clear,
+                    store: DontCare,
+                    format: IMAGE_FORMAT,
+                    samples: MSAA_SAMPLES,
+                },
+                color: {
+                    load: DontCare,
+                    store: Store,
+                    format: IMAGE_FORMAT,
+                    samples: 1,
+                }
+            },
+            pass: {
+                color: [intermediary],
+                depth_stencil: {},
+                resolve: [color]
+            }
+        )
+        .unwrap();
+
+        let images = PbrSceneImages::new(
+            render_pass.clone(),
+            resources,
+            &viewport,
+            MSAA_SAMPLES,
+            IMAGE_FORMAT,
+        );
+
+        let vertex_buffer = CpuAccessibleBuffer::from_iter(
+            &resources.memory_allocator,
+            BufferUsage {
+                vertex_buffer: true,
+                ..BufferUsage::empty()
+            },
+            false,
+            [
+                SceneVertex {
+                    position: [-0.98, -0.98],
+                    color: [1.0, 0.0, 0.0, 1.0],
+                },
+                SceneVertex {
+                    position: [-0.98, 0.98],
+                    color: [0.0, 1.0, 0.0, 1.0],
+                },
+                SceneVertex {
+                    position: [0.98, -0.98],
+                    color: [0.0, 0.0, 1.0, 1.0],
+                },
+                SceneVertex {
+                    position: [0.98, -0.98],
+                    color: [1.0, 0.0, 0.0, 1.0],
+                },
+                SceneVertex {
+                    position: [-0.98, 0.98],
+                    color: [1.0, 0.0, 0.0, 1.0],
+                },
+                SceneVertex {
+                    position: [0.8, 0.9],
+                    color: [1.0, 0.0, 0.0, 1.0],
+                },
+            ]
+            .iter()
+            .cloned(),
+        )
+        .expect("failed to create buffer");
+
+        let subpass = Subpass::from(render_pass.clone(), 0).unwrap();
+
+        let vs = vs::load(device.clone()).unwrap();
+        let fs = fs::load(device.clone()).unwrap();
+
+        let pipeline = GraphicsPipeline::start()
+            .vertex_input_state(BuffersDefinition::new().vertex::<SceneVertex>())
+            .vertex_shader(vs.entry_point("main").unwrap(), ())
+            .input_assembly_state(
+                InputAssemblyState::new().topology(PrimitiveTopology::TriangleList),
+            )
+            .rasterization_state(RasterizationState {
+                front_face: StateMode::Fixed(FrontFace::CounterClockwise),
+                cull_mode: StateMode::Fixed(CullMode::None),
+                ..RasterizationState::default()
+            })
+            .multisample_state(MultisampleState {
+                rasterization_samples: MSAA_SAMPLES,
+                ..Default::default()
+            })
+            .depth_stencil_state(DepthStencilState {
+                depth: Some(DepthState {
+                    enable_dynamic: false,
+                    write_enable: StateMode::Fixed(true),
+                    compare_op: StateMode::Fixed(CompareOp::Greater),
+                }),
+                ..DepthStencilState::default()
+            })
+            .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
+            .fragment_shader(fs.entry_point("main").unwrap(), ())
+            .render_pass(subpass.clone())
+            .build(device.clone())
+            .unwrap();
+
+        Self {
+            clear_color,
+            render_pass,
+            pipeline,
+            subpass,
+            vertex_buffer,
+            viewport,
+            images,
+            msaa_samples,
+        }
+    }
+
+    fn update_viewport<'a>(&mut self, info: &PaintCallbackInfo, resources: &RenderResources<'a>) {
+        let vp = SceneViewport::from_info(info);
+        if vp != self.viewport {
+            self.viewport = vp;
+            self.images = PbrSceneImages::new(
+                self.render_pass.clone(),
+                resources,
+                &self.viewport,
+                MSAA_SAMPLES,
+                IMAGE_FORMAT,
+            )
+        }
+    }
+}
+impl Scene for PbrScene {
+    fn render<'a>(&mut self, info: &PaintCallbackInfo, resources: &RenderResources<'a>) {
+        self.update_viewport(info, resources);
+
+        let mut scene_builder = AutoCommandBufferBuilder::primary(
+            resources.command_buffer_allocator,
+            resources.queue.queue_family_index(),
+            CommandBufferUsage::MultipleSubmit,
+        )
+        .unwrap();
+
+        scene_builder
+            .begin_render_pass(
+                RenderPassBeginInfo {
+                    clear_values: vec![Some(self.clear_color.into()), None],
+                    render_area_offset: self.viewport.origin,
+                    render_area_extent: self.viewport.dimensions,
+                    ..RenderPassBeginInfo::framebuffer(self.images.framebuffer.clone())
+                },
+                SubpassContents::Inline,
+            )
+            .unwrap()
+            .set_viewport(0, [self.viewport.to_vulkan_viewport()])
+            .bind_pipeline_graphics(self.pipeline.clone())
+            .bind_vertex_buffers(0, self.vertex_buffer.clone())
+            .draw(self.vertex_buffer.len() as u32, 1, 0, 0)
+            .unwrap()
+            .end_render_pass()
+            .unwrap();
+
+        let command_buffer = scene_builder.build().unwrap();
+
+        let finished = command_buffer.execute(resources.queue.clone()).unwrap();
+        finished
+            .then_signal_fence_and_flush()
+            .unwrap()
+            .wait(None)
+            .unwrap();
+    }
+
+    fn view(&self) -> Arc<dyn ImageViewAbstract> {
+        self.images.view.clone()
+    }
+}
+
+struct PbrSceneImages {
+    framebuffer: Arc<Framebuffer>,
+    intermediary: Arc<ImageView<AttachmentImage>>,
+    view: Arc<ImageView<StorageImage>>,
+}
+impl PbrSceneImages {
+    fn new(
+        scene_render_pass: Arc<RenderPass>,
+        resources: &RenderResources,
+        viewport: &SceneViewport,
+        samples: SampleCount,
+        format: Format,
+    ) -> Self {
+        // Make sure the images are at least 1 pixel in each dimension or Vulkan will
+        // throw an error. Also make the images cover the offset area so they line up
+        // with the egui area that we're painting. We'll only render to the area that
+        // will be shown by egui.
+        //
+        // TODO: Is there a way to do this that doesn't waste memory on the offset area?
+        let dimensions = [
+            match viewport.dimensions[0] > 0 {
+                true => viewport.dimensions[0],
+                false => 1,
+            } + viewport.origin[0],
+            match viewport.dimensions[1] > 0 {
+                true => viewport.dimensions[1],
+                false => 1,
+            } + viewport.origin[1],
+        ];
+
+        let intermediary = ImageView::new_default(
+            AttachmentImage::multisampled(&resources.memory_allocator, dimensions, samples, format)
+                .unwrap(),
+        )
+        .unwrap();
+
+        let image = StorageImage::new(
+            &resources.memory_allocator,
+            ImageDimensions::Dim2d {
+                width: dimensions[0],
+                height: dimensions[1],
+                array_layers: 1,
+            },
+            format,
+            Some(resources.queue.queue_family_index()),
+        )
+        .unwrap();
+
+        let view = ImageView::new_default(image.clone()).unwrap();
+
+        let framebuffer = Framebuffer::new(
+            scene_render_pass,
+            FramebufferCreateInfo {
+                attachments: vec![intermediary.clone(), view.clone()],
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        Self {
+            framebuffer,
+            intermediary,
+            view,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Default, Debug, Copy, Clone, Zeroable, Pod)]
+struct SceneVertex {
+    position: [f32; 2],
+    color: [f32; 4],
+}
+vulkano::impl_vertex!(SceneVertex, position, color);
+
+mod vs {
+    vulkano_shaders::shader! {
+        ty: "vertex",
+        src: "
+#version 450
+layout(location = 0) in vec2 position;
+layout(location = 1) in vec4 color;
+
+layout(location = 0) out vec4 v_color;
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+    v_color = color;
+}"
+    }
+}
+
+mod fs {
+    vulkano_shaders::shader! {
+        ty: "fragment",
+        src: "
+#version 450
+layout(location = 0) in vec4 v_color;
+
+layout(location = 0) out vec4 f_color;
+
+void main() {
+    f_color = v_color;
+}"
+    }
+}

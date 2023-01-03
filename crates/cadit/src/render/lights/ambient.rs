3a3c5096d38cf7034da2c031c72df9b20b10a5de
Ross Tollefson <past9sys@gmail.com>
2023-01-02 22:13:30 -0700

    Ambient and directional light structs

--- crates/cadit/src/render/lights/ambient.rs
+++ crates/cadit/src/render/lights/ambient.rs
@@ -1,6 +1,36 @@
+use std::sync::Arc;
+
+use crevice::std140::AsStd140;
+use vulkano::{
+    buffer::{BufferUsage, CpuAccessibleBuffer},
+    memory::allocator::MemoryAllocator,
+};
+
 use crate::render::Color;
 
+#[derive(AsStd140, Clone, Debug)]
 pub struct AmbientLight {
     color: Color,
     intensity: f32,
+}
+impl AmbientLight {
+    pub fn new(color: Color, intensity: f32) -> Self {
+        Self { color, intensity }
+    }
+
+    pub fn buffer(
+        allocator: &(impl MemoryAllocator + ?Sized),
+        lights: &[AmbientLight],
+    ) -> Arc<CpuAccessibleBuffer<[Std140AmbientLight]>> {
+        CpuAccessibleBuffer::from_iter(
+            allocator,
+            BufferUsage {
+                storage_buffer: true,
+                ..BufferUsage::default()
+            },
+            false,
+            lights.iter().map(|light| light.as_std140()),
+        )
+        .unwrap()
+    }
 }
--- crates/cadit/src/render/lights/directional.rs
+++ crates/cadit/src/render/lights/directional.rs
@@ -1,7 +1,41 @@
+use std::sync::Arc;
+
+use crevice::std140::AsStd140;
+use vulkano::{
+    buffer::{BufferUsage, CpuAccessibleBuffer},
+    memory::allocator::MemoryAllocator,
+};
+
 use crate::render::{cgmath_types::Vec3, Color};
 
+#[derive(AsStd140, Clone, Debug)]
 pub struct DirectionalLight {
     direction: Vec3,
     color: Color,
     intensity: f32,
+}
+impl DirectionalLight {
+    pub fn new(direction: Vec3, color: Color, intensity: f32) -> Self {
+        Self {
+            direction,
+            color,
+            intensity,
+        }
+    }
+
+    pub fn buffer(
+        allocator: &(impl MemoryAllocator + ?Sized),
+        lights: &[DirectionalLight],
+    ) -> Arc<CpuAccessibleBuffer<[Std140DirectionalLight]>> {
+        CpuAccessibleBuffer::from_iter(
+            allocator,
+            BufferUsage {
+                storage_buffer: true,
+                ..BufferUsage::default()
+            },
+            false,
+            lights.iter().map(|light| light.as_std140()),
+        )
+        .unwrap()
+    }
 }
